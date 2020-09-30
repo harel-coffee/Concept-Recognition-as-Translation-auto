c959d6046f64217a28a15ff63a4f2d56645cce58
mboguslav <mayla.boguslav@cuanschutz.edu>
2020-09-30 13:42:42 -0500

    added comments

--- Code/fiji_run_open_nmt_full.sh
+++ Code/fiji_run_open_nmt_full.sh
@@ -1,65 +1,66 @@
 #!/usr/bin/env bash
 source bin/activate
 
+##all ontologies
 #ontologies='CHEBI,CL,GO_BP,GO_CC,GO_MF,MOP,NCBITaxon,PR,SO,UBERON'
-#ontologies='CHEBI'
+##one ontology at a time for speed - parallelization
 ont='CHEBI'
 
+##list of excluded files from training
 excluded_files='11532192,17696610'
 
+##Path to the BIO- format tokenized files that were preprocessed
 tokenized_file_path='/Users/mabo1182/negacy_project/Tokenized_Files/'
 
-#save_models_path='/Users/MaylaB/Dropbox/Documents/0_Thesis_stuff-Larry_Sonia/Negacy_seq_2_seq_NER_model/ConceptRecognition/PythonScripts/models/'
-
+##path to the concept norm file path
 concept_norm_files_path='/Users/mabo1182/negacy_project/Concept_Norm_Files'
-
 full_files_path='full_files/'
 
-##PREPROCESS ALL ONTOLOGY INFORMATION
+
+##preprocess all ontology information for concept normalization if not already done
 python3 concept_normalization_preprocess_full.py -tokenized_file_path=$tokenized_file_path -excluded_files=$excluded_files -ontologies=$ont -concept_norm_files_path=$concept_norm_files_path -full_files_path=$full_files_path
 
 
 
 
-
-#data_files='/Users/mabo1182/negacy_project/Concept_Norm_Files'
-
-#output_files='/Users/mabo1182/negacy_project/Concept_Norm_Files/CHEBI/seq_2_seq_output/'
-
+##filename endings of all the different files for openNMT
+#character source file with the text spans
 src_file='_combo_src_file_char.txt'
 
+#character target file with the concept IDs
 tgt_file='_combo_tgt_concept_ids_char.txt'
 
+#character source validation file with the text spans
 src_val='_combo_src_file_char_val.txt'
 
+#character target validation file with the concept IDs
 tgt_val='_combo_tgt_concept_ids_char_val.txt'
 
+#the output filename
 seq_2_seq_output='seq_2_seq_output'
 
-#ont='CHEBI'
-
+##output the name of the ontology for progress report
 echo "CURRENT ONTOLOGY: $ont"
 
-#declare -a arr=('CHEBI' 'CL' 'GO_BP' 'GO_CC' 'GO_MF' 'MOP' 'NCBITaxon' 'PR' 'SO' 'UBERON')
 
+##the filenames of the different experiments for concept normalization 
 declare -a arr=('no_duplicates' 'random_ids' 'shuffled_ids' 'alphabetical')
-#declare -a arr=('no_duplicates') #'random_ids' 'shuffled_ids')
 
+##the model name for OpenNMT
 declare -a mod=('model-char_step_100000')
 
-#char_file='_combo_src_file_char_val.txt'
-#char_file='_combo_src_file_char.txt'
 
+##filenames for the output predictions
 pred_files='predictions'
-
 char_file_output='_pred.txt'
 char_file_output_val='_val_pred.txt'
 
 
+##Number of threads to use for GPUs
 export OMP_NUM_THREADS=16
 
 
-## now loop through the above array
+## now loop through the above array of filenames for the different experiments
 for i in "${arr[@]}"
 do
     echo $i
@@ -71,13 +72,13 @@ do
         #train the model
         onmt_train -data $concept_norm_files_path/$ont/$i/$seq_2_seq_output/$ont-char -save_model $concept_norm_files_path/$ont/$i/$seq_2_seq_output/$ont-model-char -model_type='text' -encoder_type='rnn' -decoder_type='rnn' -rnn_type='LSTM' -save_checkpoint_steps=5000 -valid_steps=10000 -train_steps=100000 -early_stopping=10000 -optim='sgd' #--world_size 1 #--gpu_ranks 0
 
-        ##runs the opennmt model
+        ##runs the opennmt model to translate/predict
         for j in "${mod[@]}"
         do
             #training data predictions
             onmt_translate -model $concept_norm_files_path/$ont/$i/$seq_2_seq_output/$ont-$j.pt -src $concept_norm_files_path/$ont/$i/$ont$src_file -output $concept_norm_files_path/$ont/$i/$pred_files/$ont-$j$char_file_output -replace_unk #--gpu 0 #-verbose #$i/
 
-            ##TODO: run report
+            #run an evaluation report on the training data predictions
             analysis_type=''
             python3 open_nmt_evaluation.py -ontologies=$ont -concept_norm_path=$concept_norm_files_path -truth_path=$i -predicted_path=$pred_files -analysis_type=$analysis_type
 
@@ -87,7 +88,7 @@ do
             onmt_translate -model $concept_norm_files_path/$ont/$i/$seq_2_seq_output/$ont-$j.pt -src $concept_norm_files_path/$ont/$i/$ont$src_val -output $concept_norm_files_path/$ont/$i/$pred_files/$ont-$j$char_file_output_val -replace_unk #--gpu 0 #-verbose #$i/
 
 
-            ##TODO: run report
+            #run an evaluation report on the validation data predictions
             analysis_type='val'
             python3 open_nmt_evaluation.py -ontologies=$ont -concept_norm_path=$concept_norm_files_path -truth_path=$i -predicted_path=$pred_files -analysis_type=$analysis_type
 
