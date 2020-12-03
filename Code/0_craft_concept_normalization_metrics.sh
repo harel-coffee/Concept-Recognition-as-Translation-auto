a95193b8a58bbf9de48186df1e220610239b29bc
mboguslav <mayla.boguslav@cuanschutz.edu>
2020-12-03 12:26:09 -0700

    revision update: evaluating concept normalization for 30 held out document evaluation set

--- Code/0_craft_concept_normalization_metrics.sh
+++ Code/0_craft_concept_normalization_metrics.sh
@@ -9,13 +9,13 @@ gold_standard_bionlp_path='/craft-st-2019/gold-annotations/craft-ca/'
 ##all ontologies of interest
 ontologies='CHEBI,CL,GO_BP,GO_CC,GO_MF,MOP,NCBITaxon,PR,SO,UBERON'
 ##output path for concept normalization metrics results
-output_path='.../Concept-Recognition-as-Translation/Evaluation_Files/Concept_Norm_Files/'
+output_path='.../Concept-Recognition-as-Translation/Output_Folders/Evaluation_Files/Concept_Norm_Files/'
 
 ##all experiments with no duplicates first since it is a reference for the rest!
 experiments='no_duplicates,random_ids,shuffled_ids,alphabetical'
 
 ##concept norm file path for all the predictions
-concept_norm_file_path='.../Concept-Recognition-as-Translation/Concept_Norm_Files/'
+concept_norm_file_path='.../Concept-Recognition-as-Translation/Output_Folders/Concept_Norm_Files/'
 
 ##set up the gold standard files to be able to evaluate everything - includes experiments
 python3 gs_spans_for_concept_normalization.py -evaluation_files=$evaluation_files -gold_standard_bionlp_path=$gold_standard_bionlp_path -ontologies=$ontologies -output_path=$output_path --experiments=$experiments --concept_norm_file_path=$concept_norm_file_path
@@ -24,7 +24,7 @@ python3 gs_spans_for_concept_normalization.py -evaluation_files=$evaluation_file
 
 ##run the concept normalization pipeline (eval_open_nmt)
 ##evaluation path with folders
-eval_path='.../Concept-Recognition-as-Translation/Evaluation_Files/'
+eval_path='.../Concept-Recognition-as-Translation/Output_Folders/Evaluation_Files/'
 concept_norm_files='Concept_Norm_Files/'
 results_concept_norm_files='Results_concept_norm_files/'
 
@@ -55,12 +55,12 @@ for i in "${ont[@]}"
 
 ##calculate metrics for the predicted vs the gold standard
 ##gold standard and predicted paths
-gold_standard_path='.../Concept-Recognition-as-Translation/Evaluation_Files/Concept_Norm_Files/'
-predicted_path='.../Concept-Recognition-as-Translation/Evaluation_Files/Results_concept_norm_files/'
+gold_standard_path='.../Concept-Recognition-as-Translation/Output_Folders/Evaluation_Files/Concept_Norm_Files/'
+predicted_path='.../Concept-Recognition-as-Translation/Output_Folders/Evaluation_Files/Results_concept_norm_files/'
 ##concept id file suffix with the concepts for both predicted and gold standard
 concept_id_file='_combo_tgt_concept_ids_char'
 ##output path for the metrics files
-output_path='.../Concept-Recognition-as-Translation/concept_system_output/'
+output_path='.../Concept-Recognition-as-Translation/Output_Folders/concept_system_output/'
 ##filename prefixes for each gold standard and prediction file respectively
 gs_file_name='_combo_tgt_concept_ids_char'
 pred_file_name='-model-char_step_100000_pred'
@@ -74,7 +74,7 @@ python3 calculate_concept_normalization_metrics.py -ontologies=$ontologies -gold
 ##Run the same pipeline for each experiment done for only the core set for each ontology
 ##loop over all experiments to run and get output on gold standard
 ##experiment output folder
-experiment_output='.../Concept-Recognition-as-Translation/Results_concept_norm_files/'
+experiment_output='.../Concept-Recognition-as-Translation/Output_Folders/Results_concept_norm_files/'
 ##all experiments
 declare -a arr=('no_duplicates' 'random_ids' 'shuffled_ids' 'alphabetical')
 ##folder with the model
@@ -106,7 +106,7 @@ python3 calculate_concept_normalization_metrics.py -ontologies=$ontologies -gold
 ##calculate metrics for ConceptMapper comparison - baseline comparison
 experiments='conceptmapper'
 conceptmapper_path='.../Concept-Recognition-as-Translation/Concept-Recognition-as-Translation-ConceptMapper-Baseline/data/output/core/'
-gold_standard_path='.../Concept-Recognition-as-Translation/Evaluation_Files/Concept_Norm_Files/'
+gold_standard_path='.../Concept-Recognition-as-Translation/Output_Folders/Evaluation_Files/Concept_Norm_Files/'
 
 python3 calculate_concept_normalization_metrics.py -ontologies=$ontologies -gold_standard_path=$gold_standard_path -gs_file_name=$gs_file_name -predicted_path=$conceptmapper_path -pred_file_name=$pred_file_name -output_path=$output_path --experiments=$experiments
 
