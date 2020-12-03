dc506da0b1d4479899ed16c0f14a22307ac9e380
mboguslav <mayla.boguslav@cuanschutz.edu>
2020-12-03 09:54:46 -0700

    updated for revisions - gathers concepts from .obo files better

--- Code/concept_normalization_obo_addition.py
+++ Code/concept_normalization_obo_addition.py
@@ -8,7 +8,7 @@ def read_obo_file(obo_file_path, ontology, GO_dict, extensions):
 
     ontology_concept_dict = {} #ontology_concept_id -> [name, def, [synonyms]]
 
-    ##gather all unused concepts
+    ##gather all unused concepts - filepath for extensions vs not
     if extensions:
         #unused_classes_and_substitute_extension_classes_for_CHEBI+extensions_annotations.txt
         unused_concept_file_path = '%s%s/%s/unused_classes_and_substitute_extension_classes_for_%s_annotations.txt' % (obo_file_path, ontology.replace('_EXT', ''), ontology.replace('_EXT', '+extensions'), ontology.replace('_EXT', '+extensions'))
@@ -19,17 +19,23 @@ def read_obo_file(obo_file_path, ontology, GO_dict, extensions):
 
 
     unused_concept_list = []
+    unused_concept_dict = {} #only concepts that are the same but with _EXT added
 
-
-
+    ##gather all unused concepts so we don't use them
     with open(unused_concept_file_path, 'r') as unused_concepts_file:
         for line in unused_concepts_file:
+            line_list = line.strip('\n').split('\t')
             if extensions:
-                unused_concept_list += [line.strip('\n').split('\t')[0]]
+                unused_concept_list += [line_list[0]]
+                if len(line.split('\t')) == 2 and line_list[0] == line_list[1].replace('_EXT', ''):
+                    unused_concept_dict[line_list[0]] = line_list[1]
+                else:
+                    pass
             else:
                 unused_concept_list += [line.strip('\n')]
 
-    ##only take the namespace that is correct. For go look at the namespace category also
+    ##grab all obo concepts from .obo files for each ontology - obo file path
+    ##only take the namespace that is correct. For GO look at the namespace category also
     if 'GO' in ontology:
         if extensions:
             if 'GO_MF' in ontology:
@@ -43,44 +49,58 @@ def read_obo_file(obo_file_path, ontology, GO_dict, extensions):
     else:
         if extensions:
             obo_file_path_full = '%s%s/%s/%s.obo.gz' % (obo_file_path, ontology.replace('_EXT',''), ontology.replace('_EXT', '+extensions'), ontology.replace('_EXT', '+extensions'))
+            # print(obo_file_path_full)
         else:
             obo_file_path_full = '%s%s/%s/%s.obo.gz' %(obo_file_path, ontology, ontology, ontology)
 
 
-
-
+    ##gather all concepts from .obo file that are relevant to the ontology
     with gzip.open(obo_file_path_full, 'rb') as obo_file:
         current_concept = None
         ##file in bytes and need to convert to strings - decode(utf-8)
+        valid_term = False
         for line in obo_file:
+
             ##obofile: term, id, name, (namespace), def, synonym
             str_line = line.decode('utf-8').strip('\n')
 
             ##capture the term
             if '[Term]' == str_line:
-                pass
+                valid_term = True
 
-            elif str_line.startswith('id:'):
+            ##concept id
+            elif valid_term and str_line.startswith('id:'):
+
+                ##determine the current concept id with extensions caveat
                 if ontology_concept_dict.get(str_line.split(' ')[-1]):
+                    print(str_line.split(' ')[-1])
+                    print(ontology_concept_dict[str_line.split(' ')[-1]])
                     raise Exception('ERROR WITH ONTOLOGY DICT INITIALIZING!')
-                elif str_line.split(' ')[-1] not in unused_concept_list:
+                elif extensions and unused_concept_dict.get(str_line.split(' ')[-1]):
+                    current_concept = unused_concept_dict[str_line.split(' ')[-1]]
+                elif str_line.split(' ')[-1] not in unused_concept_list and (ontology.replace('_EXT', '') in str_line.split(' ')[-1].split(':')[0] or ('GO' in ontology and 'GO' in str_line.split(' ')[-1].split(':')[0])):
                     current_concept = str_line.split(' ')[-1]
-                    if current_concept.split(':')[0] == ontology:
-                        ontology_concept_dict[str_line.split(' ')[-1]] = [None, None, []]
-                    elif 'GO' in ontology and current_concept.split(':')[0] == 'GO':
-                        ##need to also check the namespace category to see if CC, MF, or BP
-                        ontology_concept_dict[str_line.split(' ')[-1]] = [None, None, []]
-                    else:
-                        ##making sure to take the correct name space for the ontology
-                        current_concept = None
-                        pass
+                else:
+                    current_concept = None
+
+
+                ##initialize the ontology_concept_dict
+                if current_concept:
+                    ontology_concept_dict[current_concept] = [None, None, []]
+
+
                 else:
                     ##unused concepts
                     pass
 
-            elif current_concept and str_line.startswith('name:'):
-                ontology_concept_dict[current_concept][0] = str_line[len('name: '): ]
+                ##reset the valid term for next round
+                valid_term = False
 
+            ##name of concept
+            elif current_concept and str_line.startswith('name:'):
+                ontology_concept_dict[current_concept][0] = str_line[len('name: '):]
+
+            ##for GO need namespace to be correct ontology (MF, BP, CC)
             elif current_concept and 'GO' in ontology and str_line.startswith('namespace:'):
                 name_space = str_line.split(': ')[-1]
                 ##need to make sure we have the correct namespace for GO: BP, CC, MF
@@ -90,10 +110,11 @@ def read_obo_file(obo_file_path, ontology, GO_dict, extensions):
                     continue
 
 
-
+            ##definition of concept
             elif current_concept and str_line.startswith('def:'):
                 ontology_concept_dict[current_concept][1] = str_line.split('"')[1]
 
+            ##synonyms of concept - only take if Exact or special for PR
             elif current_concept and str_line.startswith('synonym:'):
                 synonym_info = str_line.split('"')[2]
                 ##only take exact synonyms except for PR take the related synonyms also
@@ -102,7 +123,7 @@ def read_obo_file(obo_file_path, ontology, GO_dict, extensions):
                 if 'EXACT' in synonym_info:
                     ##or ('RELATED' in synonym_info and 'InChI' not in synonym_info and 'InChIKey' not in synonym_info) and str_line.split('"')[1] != '.'
                     ontology_concept_dict[current_concept][2] += [str_line.split('"')[1]]
-                elif ontology == 'PR' and 'RELATED' in synonym_info:
+                elif (ontology == 'PR_EXT' or ontology == 'PR') and 'RELATED' in synonym_info:
                     ontology_concept_dict[current_concept][2] += [str_line.split('"')[1]]
 
 
@@ -114,17 +135,20 @@ def read_obo_file(obo_file_path, ontology, GO_dict, extensions):
 
 
 def output_ontology_addition_file(ontology, ontology_concept_dict, concept_norm_files_path):
+    ##output the additional obo concepts
     with open('%s%s/%s_addition.txt' %(concept_norm_files_path, ontology, ontology), 'w+') as additional_ont_concepts_file:
         additional_ont_concepts_file.write('%s\t%s\n' %(ontology, len(ontology_concept_dict)))
-
 
         ##ontology_concept_id -> [name, def, [synonyms]]
         for ont_id in ontology_concept_dict.keys():
             [name, definition, synonyms] = ontology_concept_dict[ont_id]
-            if 'OBSOLETE' not in definition:
-                additional_ont_concepts_file.write('%s\t%s\t%s\t%s\n' %(ont_id, name, definition, synonyms))
-            else:
+
+            ##not everything has a definition and that is okay!
+            if definition and 'OBSOLETE' in definition:
                 pass
+            else:
+                additional_ont_concepts_file.write('%s\t%s\t%s\t%s\n' %(ont_id, name, definition, synonyms))
+
 
 
 
@@ -143,15 +167,17 @@ if __name__ == '__main__':
 
     ontologies = args.ontologies.split(',')
 
+    ##GO is weird with namespaces so we help it
     GO_dict = {'GO_BP':'biological_process', 'GO_CC':'cellular_component', 'GO_MF':'molecular_function', 'GO_BP_EXT':'biological_process', 'GO_CC_EXT':'cellular_component', 'GO_MF_EXT':'molecular_function'}
 
-
+    ##grab all additional obo concepts for each ontology
     for ontology in ontologies:
         print('PROGRESS:', ontology)
 
+        ##gather all ontology concepts from the .obo file
         ##ontology_concept_id -> [name, def, [synonyms]]
         ontology_concept_dict = read_obo_file(args.obo_file_path, ontology, GO_dict, args.extensions)
 
-        ##output the files to the concept_normalized spot to use to train for normalization
+        ##output the files to the concept_normalized spot to use to train for concept normalization
         output_ontology_addition_file(ontology, ontology_concept_dict, args.concept_norm_files_path)
 
