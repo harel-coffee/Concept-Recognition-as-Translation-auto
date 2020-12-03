3f84be73b6accf3d3d9356d1b989c35e48851a5f
mboguslav <mayla.boguslav@cuanschutz.edu>
2020-12-03 11:14:45 -0700

    added comments

--- Code/0_craft_run_eval_biobert_pipeline_1.5.sh
+++ Code/0_craft_run_eval_biobert_pipeline_1.5.sh
@@ -1,74 +1,67 @@
 #!/usr/bin/env bash
 
 
-
+#craft evaluation path
 craft_st_path='/craft-st-2019/'
+##path to all files
 concept_recognition_path='../Concept-Recognition-as-Translation/'
-
+##evaluation path
 eval_path='/Output_Folders/Evaluation_Files/'
-
+##concept system output path
 concept_system_output='concept_system_output/'
-
+##All folders for results
 article_folder='Articles/txt/' #want files.txt
 tokenized_files='Tokenized_Files/'
 results_span_detection='Results_span_detection/'
 concept_norm_files='Concept_Norm_Files/'
 pmcid_sentence_files_path='PMCID_files_sentences/'
-
+##annotation folder
 concept_annotation='concept-annotation/'
-
+##all ontologies of interest
 ontologies="CHEBI,CL,GO_BP,GO_CC,GO_MF,MOP,NCBITaxon,PR,SO,UBERON"
-
+##pmcid evaluation files
 evaluation_files="11319941,11604102,14624252,14675480,14691534,15018652,15070402,15238161,15328538,15560850,15615595,15619330,15784609,15850489,15882093,16026622,16027110,16410827,16517939,16611361,16787536,16800892,16968134,17029558,17201918,17206865,17465682,17503968,17565376,17677002"
 
-
+##is there a gold standard or not for evaluation
 gold_standard='false'
 
-algos='BIOBERT' ##CRF, LSTM, LSTM_CRF, CHAR_EMBEDDINGS, LSTM_ELMO, BIOBERT
+##the algorithm we are focusing on - specifically BioBERT due to running
+algos='BIOBERT'  ##CRF, LSTM, LSTM_CRF, CHAR_EMBEDDINGS, LSTM_ELMO, BIOBERT
 
 
 
 ##FOR BIOBERT
 biobert='BIOBERT'
 if [ $algos == $biobert ]; then
-#    ##move test.tsv file to fiji for predictions
-#    prediction_file='/Users/MaylaB/Dropbox/Documents/0_Thesis_stuff-Larry_Sonia/Negacy_seq_2_seq_NER_model/ConceptRecognition/Evaluation_Files/Tokenized_Files/BIOBERT'
-#    scp $prediction_file/test.tsv mabo1182@fiji.colorado.edu:/Users/mabo1182/negacy_project/Evaluation_Files/Tokenized_Files/BIOBERT/
-#
-#    #GO TO FIJI_RUN_EVAL_BIOBERT!
-#    ##TODO: biobert run classification algorithm - fiji_run_eval_biobert.sh on fiji!!!
-#    #sbatch GPU_run_fiji_eval_biobert.sbatch - runs fiji_run_eval_biobert.sh
-
-
-    ##BRING ALL OUTPUT LOCALLY FOR BIOBERT
+    ##0. you have just run the BioBERT models on fiji for span detection
+    ##1. Bring the results files local for each ontology - all files with "*_test.txt*"
     declare -a arr=('CHEBI' 'CL' 'GO_BP' 'GO_CC' 'GO_MF' 'MOP' 'NCBITaxon' 'PR' 'SO' 'UBERON')
-##    declare -a arr=('CHEBI')
-#
-#    ##loop over each ontology and run the corresponding model
+
+    ##loop over each ontology and run the corresponding model
     results_span_detection='/Output_Folders/Evaluation_Files/Results_span_detection/'
     biobert='/BIOBERT'
     local_results='/Output_Folders/Evaluation_Files/Results_span_detection/'
-#
-#    for i in "${arr[@]}"
-#    do
-#        echo $i
-#        results_path=$results_span_detection$i$biobert
-#        local_path=$local_results$i$biobert
-#        scp mabo1182@fiji.colorado.edu:$results_path/*_test.txt* $local_path
-#
-#    done
+    
+    ##Grab all files with "*_test.txt*" local with fiji 
+    for i in "${arr[@]}"
+    do
+       echo $i
+       results_path=$results_span_detection$i$biobert
+       local_path=$local_results$i$biobert
+       # scp USERNAME@fiji.colorado.edu:$results_path/*_test.txt* $local_path
 
-    ###ner_detokenize_updated for predictions only
-    #updated detokenize to put all stuff back together to CONLL format!
+    done
+
+
+    ##2. Detokenize all BioBERT results files (updated the detokenize script)
 
     biotags='B,I,O-,O' #ordered for importance
     gold_standard='false'
-#    gold_standard='true'
     true='true'
 
     declare -a ont=('CHEBI' 'CL' 'GO_BP' 'GO_CC' 'GO_MF' 'MOP' 'NCBITaxon' 'PR' 'SO' 'UBERON')
-#    declare -a ont=('UBERON')
-#    declare -a ont=('GO_MF')
+
+    ##loop over each ontology and reformat the BioBERT output files to match the input
     for i in "${ont[@]}"
     do
         echo "$i"
@@ -77,6 +70,7 @@ if [ $algos == $biobert ]; then
         NER_DIR=$eval_path$tokenized_files$biobert
         OUTPUT_DIR=$eval_path$results_span_detection$i$biobert
 
+        ##detokenize the bioBERT results files
         python3 biobert_ner_detokenize_updated.py --token_test_path=$OUTPUT_DIR/token_test.txt --label_test_path=$OUTPUT_DIR/label_test.txt --answer_path=$NER_DIR/test.tsv --output_dir=$OUTPUT_DIR --biotags=$biotags --gold_standard=$gold_standard
 
         echo 'DONE WITH TEST.TSV'
@@ -89,13 +83,11 @@ if [ $algos == $biobert ]; then
 
 
             ##classification report if gold standard
-
             python3 biobert_classification_report.py --ner_conll_results_path=$OUTPUT_DIR/ --biotags=$biotags --ontology=$i --output_path=$OUTPUT_DIR/ --gold_standard=$gold_standard
 
             #copy the classification report to the main results with ontology name
             biobert_class_report='_biobert_local_eval_files_classification_report.txt'
             cp $OUTPUT_DIR/biobert_classification_report.txt $eval_path$results_span_detection$i/$i$biobert_class_report
-
 
 
         fi
@@ -108,7 +100,6 @@ if [ $algos == $biobert ]; then
     biobert_prediction_results=$eval_path$results_span_detection
 
     ##create the evaluation dataframe!
-#    ontologies='CL'
     python3 biobert_eval_dataframe_output.py -ontologies=$ontologies -excluded_files=$evaluation_files -tokenized_file_path=$eval_path$tokenized_files -biobert_prediction_results=$biobert_prediction_results -output_path=$biobert_prediction_results -algos=$algos --pmcid_sentence_files_path=$pmcid_sentence_files_path
 
 fi
